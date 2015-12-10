67e5e67033bbb45d9aad16446e632cd0ef23ad31
Astarenides <astarenides@hotmail.com>
2015-12-10 18:33:53 +0100

    Blog finished

--- zend/module/Blog/src/Blog/Mapper/PostMapperInterface.php
+++ zend/module/Blog/src/Blog/Mapper/PostMapperInterface.php
@@ -25,4 +25,12 @@ interface PostMapperInterface
      * @throws \Exception
      */
     public function save(PostInterface $postObject);
+    
+    /**
+     * @param PostInterface $postObject
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function delete(PostInterface $postObject);
 }
